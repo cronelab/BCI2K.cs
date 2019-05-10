a6b31e1915d0e0a0b247544cc1e9ea9a31dfb659
Chris <17508521+TheBrainChain@users.noreply.github.com>
2019-05-09 23:28:42 -0400

    Initial JS -> CS commit

---
+++ Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCI2K.cs;
+using WebSocketSharp;
+namespace Example
+{
+    class Program
+    {
+        public static Operator bci = new Operator("ws://127.0.0.1:20100");
+        static void Main(string[] args)
+        {
+
+            bci.connect();
+            Console.Read();
+            //while (true)
+            //{
+            //    Console.WriteLine("BCI Command: ");
+            //    bci.sendMsg(Console.ReadLine());
+            //}
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BCI2K.cs
-A C# connector for BCI2000
+
+#### Work in progress
+
+Depends on WebSocketSharp
+> Mimics [BCI2K.js](https://github.com/cronelab/bci2k.js) and subsequently depends on [BCI2000Web](https://github.com/cronelab/bci2000web/tree/master/src/custom) using BCI2000's WSIOFilter
